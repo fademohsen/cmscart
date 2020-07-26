d816a3a60ed5ff6135725cc10d2553445e14629e
Fadi Mohsen <xfade@outlook.com>
2020-07-26 21:05:24 +0300

    fix

--- routes/all_products.js
+++ routes/all_products.js
@@ -1,5 +1,8 @@
 const router = require('express').Router()
 const Products = require('../models/product')
+const Category = require('../models/category')
+
+
 router.get('/' , (req,res)=>{
     Products.find({}).then(found=>{
        
@@ -8,6 +11,27 @@ router.get('/' , (req,res)=>{
     })
 
 })
+//get product by category
+router.get('/:category', function (req, res) {
+
+    var categorySlug = req.params.category;
+
+    Category.findOne({slug: categorySlug}, function (err, c) {
+
+        
+        Products.find({category:categorySlug.toLowerCase()} ,function (err, products) {
+            console.log(products);
+            if (err)
+                console.log(err);
+
+            res.render('cat_products', {
+                title: c.title,
+                products: products
+            });
+        });
+    });
+
+});
 
 
 
