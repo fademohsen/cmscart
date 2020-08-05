180abe5831abf25a7b7796b7c6774ffe559ea444
Fadi Mohsen <xfade@outlook.com>
2020-08-06 00:45:53 +0300

    update card

--- routes/cart.js
+++ routes/cart.js
@@ -55,4 +55,44 @@ router.get('/checkout' , function (req , res) {
     })
 
 })
+router.get('/update/:product' , function(req , res) {
+    var slug = req.params.product;
+    var cart = req.session.cart ;
+    var action = req.query.action ;
+
+    for (var i = 0; i < cart.length; i++) {
+        
+        if(cart[i].title == slug) {
+            switch (action) {
+                case "add":
+                    cart[i].qty++ ;
+                    break;
+                case "remove":
+                    cart[i].qty--;
+                    if(cart[i].qty < 1)
+                     cart.splice(i, 1);
+                    break;
+                case "clear":
+                    cart.splice(i , 1);
+                    if (cart.length == 0)
+                    delete req.session.cart;
+                    break;
+                    default:
+                        console.log('update problem');
+                        break ;
+
+            }
+            break;
+        }
+    }
+
+        req.flash('success' , 'Product Update')
+        res.redirect('/cart/checkout')
+})
+router.get('/clear' , function(req , res) {
+    delete req.session.cart ;
+    req.flash('success' , 'Cart Cleared')
+    res.redirect('/cart/checkout')
+
+})
 module.exports = router
